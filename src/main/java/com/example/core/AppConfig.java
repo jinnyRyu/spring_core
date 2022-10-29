a0e7ee268ae28bca469081aff111454feb077097
jinnyryu <hajinryu51@gmail.com>
2022-10-29 22:45:49 +0900

    [REF] 명확한 메서드로 ref

--- src/main/java/com/example/core/AppConfig.java
+++ src/main/java/com/example/core/AppConfig.java
@@ -2,6 +2,7 @@ package com.example.core;
 
 import com.example.core.Order.OderService;
 import com.example.core.Order.OrderServiceImpl;
+import com.example.core.discount.DscountPolicy;
 import com.example.core.discount.FixDiscountPolicy;
 import com.example.core.member.MemberService;
 import com.example.core.member.MemberServiceImpl;
@@ -10,10 +11,19 @@ import com.example.core.member.MemoryMemberRepository;
 public class AppConfig {  //app 구현체를 직접 생성하도록 
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private  MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), new FixDiscountPolicy());
+    }
+
+    public DscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+
     }
 }
